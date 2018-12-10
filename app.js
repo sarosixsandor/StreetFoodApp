a10e0a91dd61bb73e6a1efee6825fa23108336c1
Sárosi Sándor <sarosixsandor@gmail.com>
2018-12-10 19:53:41 +0100

    added ejs template files and connected them

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs")
+
+// landing page route
 app.get("/", function(req, res){
-    res.send("Landing page.");
+    res.render("landing");
+});
+
+//street food places route
+app.get("/streetfoods", function(req, res){
+    //temporary array
+    var streetfoods = [
+        {name: "ZING", picture: "https://dhalbm0yebhbn.cloudfront.net/wp-content/uploads/2016/05/2_zing_gaszromobil.hu_.jpg"},
+        {name: "PANEER", picture: "https://dhalbm0yebhbn.cloudfront.net/wp-content/uploads/2016/05/3_paneer_bebudapest.hu_.jpg"}
+    ];
+    res.render("streetfoods");
 });
 
 app.listen(3000, function(){
