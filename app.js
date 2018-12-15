2bfff14e39ac2b7ca1f14c8ada0a096e98cf513f
Sárosi Sándor <sarosixsandor@gmail.com>
2018-12-15 17:35:33 +0100

    connected mongodb using mongoose and created 1 test sf place

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/streetfood", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+// schema setup
+var streetfoodSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+// compile into model
+var Streetfood = mongoose.model("Streetfood", streetfoodSchema);
+
+Streetfood.create(
+    {
+        name: "ZING",
+        image: "https://dhalbm0yebhbn.cloudfront.net/wp-content/uploads/2016/05/2_zing_gaszromobil.hu_.jpg"
+
+    }, function(err, streetfood){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("Newly created streetfood place");
+            console.log(streetfood);
+        }
+    }
+);
 
 // temporary array replaced to be global var so it can be reached
 // this is only here temporarily, these will be placed in mongodb
