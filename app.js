73818a1d520b5d8505f4fc3b994bf5a619693097
Sárosi Sándor <sarosixsandor@gmail.com>
2018-12-16 18:36:23 +0100

    created show route to show 1 specific street food place

--- app.js
+++ app.js
@@ -10,35 +10,53 @@ app.set("view engine", "ejs");
 // schema setup (blueprint)
 var streetfoodSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 // compile into model
 var Streetfood = mongoose.model("Streetfood", streetfoodSchema);
+
+/*Streetfood.create(
+    {
+        name: "ZING",
+        image: "https://dhalbm0yebhbn.cloudfront.net/wp-content/uploads/2016/05/2_zing_gaszromobil.hu_.jpg",
+        description: "Delicious burgers made with prime cuts of beef and served up with high quality ingredients."
+    },
+    function(err, streetfood){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("created new streetfood place");
+            console.log(streetfood);
+        }
+    }
+)*/
 
 // landing page route
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-// street food places route
+// INDEX - show all street food places
 app.get("/streetfoods", function(req, res){
     // get streetfoods from db
     Streetfood.find({}, function(err, allStreetfoods){
         if(err){
             console.log(err);
         } else {
-            res.render("streetfoods", {streetfoods: allStreetfoods});
+            res.render("index", {streetfoods: allStreetfoods});
         }
     });
 });
 
-// REST post route to street food places
+// CREATE - add new street food place to DB
 app.post("/streetfoods", function(req, res){
     // get data from form and add to streetfoods[]
     var name = req.body.name;
     var image = req.body.image;
-    var newStreetfoodplace = {name: name, image: image};
+    var desc = req.body.description;
+    var newStreetfoodplace = {name: name, image: image, description: desc};
     // create a new steetfood place and save to db
     Streetfood.create(newStreetfoodplace, function(err, newlyCreated){
         if(err){
@@ -52,8 +70,21 @@ app.post("/streetfoods", function(req, res){
     });
 });
 
+// NEW - display form to create new street food place
 app.get("/streetfoods/new", function(req, res){
     res.render("new");
+});
+
+// SHOW - more info about one specific(id) street food place
+app.get("/streetfoods/:id", function(req, res){
+    var id = req.params.id;
+    Streetfood.findById(id, function(err, foundStreetfood){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("show", {streetfood: foundStreetfood})
+        }
+    });
 });
 
 app.listen(3000, function(){
