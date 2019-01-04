ec4cb8619549ff1ac24871439288ea5b970ea2bd
Sárosi Sándor <sarosixsandor@gmail.com>
2019-01-04 11:36:17 +0100

    connected css file

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/streetfood", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 // landing page route
