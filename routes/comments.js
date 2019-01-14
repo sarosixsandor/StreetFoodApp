ab5ce55b1ead2451daae8bb39d60c88553c4df3c
Sárosi Sándor <sarosixsandor@gmail.com>
2019-01-14 15:09:00 +0100

    added delete route+logic+button

--- routes/comments.js
+++ routes/comments.js
@@ -62,6 +62,17 @@ router.put("/:comment_id", function(req, res){
     });
 });
 
+// Comments - Destroy
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/streetfoods/" + req.params.id);
+        }
+    });
+});
+
 // Middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
